77bf2f75a782c38157e902933a297f9cb34e1130
Robert Kania <robert.kania@tu-berlin.de>
2016-06-14 17:15:45 +0200

    spin-off tests

--- src/test/java/de/thinkbaer/aios/tests/jdbc/HsqlDB.java
+++ src/test/java/de/thinkbaer/aios/tests/jdbc/HsqlDB.java
@@ -21,18 +21,24 @@ public class HsqlDB extends DB {
 	}
 	
 	
-
-
 	@Override
 	public void createDB(String classifier) throws Exception {
 		Connection c = getConnection();
+		update(c, "DROP TABLE  driver IF EXISTS ");
+		update(c, "DROP TABLE  car IF EXISTS ");
+		update(c, "DROP TABLE owner  IF EXISTS ");
 		update(c, "CREATE TABLE  IF NOT EXISTS car ( id INTEGER IDENTITY, type VARCHAR(256), name VARCHAR(256))");
 		update(c, "CREATE TABLE  IF NOT EXISTS owner ( id INTEGER IDENTITY, surname VARCHAR(256), givenName VARCHAR(256))");
+		update(c, "CREATE TABLE  IF NOT EXISTS driver ( id INTEGER IDENTITY, car_id INTEGER, owner_id INTEGER)");
+		update(c, "ALTER TABLE PUBLIC.driver ADD FOREIGN KEY (car_id) REFERENCES PUBLIC.car(id)");
+		update(c, "ALTER TABLE PUBLIC.driver ADD FOREIGN KEY (owner_id) REFERENCES PUBLIC.owner(id)");
 		update(c, "INSERT INTO car (type, name) VALUES('Ford', 'Mustang')");
 		update(c, "INSERT INTO car (type, name) VALUES('Ford', 'Fiesta')");
 		update(c, "INSERT INTO owner (surname,givenName) VALUES('Ford', 'Henry')");
-
+		
+		update(c, "INSERT INTO driver (car_id,owner_id) VALUES((select max(id) from car),(select max(id) from owner))");
 	}
+	
 
 	@Override
 	public Connection getConnection() throws SQLException{
